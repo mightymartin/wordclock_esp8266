affad92fbb63b933e9a3ea8238964c7996d13cb7
Martin Kraemer <martin@the-kraemer.de>
2019-03-02 21:22:05 +0100

    ldr calc improvement

--- src/ldr.cpp
+++ src/ldr.cpp
@@ -11,7 +11,7 @@ static uint8 bufferpos = 0;
 
 void LDRInit(){
     for(uint8 i = 0; i < LDR_MEASURE_COUNT-1; i++){
-        measureBuffer[i] = 1024;
+        measureBuffer[i] = 1023;
     }
     LDRTimer.start();
     LDRUpdate();
@@ -37,15 +37,26 @@ void LDRUpdate(){
     #define     TH_COUNT (sizeof(settings.l_treshold) / 2)
         
     targetVal = settings.l_min_bright;    
-    if(settings.u_LDR){        
-        for(uint8 i=0; i < TH_COUNT; i++){
-            if(LDRgetValue() >= settings.l_treshold[i]){                 
-                targetVal = settings.l_min_bright + ( (MAX_FADE_STEPS-settings.l_min_bright) * (i+1) / TH_COUNT ) - 1;                
-            }   
-            if(targetVal > MAX_FADE_STEPS-1){
-                targetVal = MAX_FADE_STEPS-1;
-            } 
-        }        
+    if(settings.u_LDR){ 
+        uint8 definedThresholdSteps = 0;
+        uint8 reachedThresholdStep = 0;
+        uint16 prevVal = 0;
+        for(uint8 i=0; i < TH_COUNT; i++){            
+            if(settings.l_treshold[i] >= prevVal){
+                prevVal = settings.l_treshold[i]; 
+
+                if(LDRgetValue() >= settings.l_treshold[i]){                 
+                    reachedThresholdStep++;
+                }            
+                definedThresholdSteps++;                
+            }                           
+        }   
+        WebLogDebug("LDRTNUM:" + String(definedThresholdSteps));
+        WebLogDebug("LDRTREA:" + String(reachedThresholdStep));
+        targetVal = settings.l_min_bright + ( (MAX_FADE_STEPS-settings.l_min_bright) * reachedThresholdStep / definedThresholdSteps ) - 1;                
+        if(targetVal > MAX_FADE_STEPS-1){
+            targetVal = MAX_FADE_STEPS-1;
+        }     
     }else{
         targetVal = MAX_FADE_STEPS-1;
     }
@@ -73,8 +84,7 @@ uint16 LDRgetValue(){
     for(uint8 i = 0; i < LDR_MEASURE_COUNT; i++){
         avgVal = avgVal + measureBuffer[i];
     }
-    avgVal = avgVal / LDR_MEASURE_COUNT;
-    return avgVal;
+    return (avgVal / LDR_MEASURE_COUNT);
 }
     
 uint8 LDRgetBrightness(){    
